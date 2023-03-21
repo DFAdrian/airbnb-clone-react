a163c3095c9fc1f412673719726e9658a4fce408
Adrian Donate <89099274+DFAdrian@users.noreply.github.com>
2023-03-21 00:18:02 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App(){
   const getData = data.map(item=>{
     return(
       <Cards 
+        key={id}
         img={item.coverImg}
         rating={item.stats.rating}
         reviewCount={item.stats.reviewCount}
